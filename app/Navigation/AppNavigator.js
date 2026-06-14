4a2509da355df4a2588d501af47eb38c08b7f73d
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-13 22:36:54 -0400

    Refactor AppNavigator to use NavigationContainer

--- app/Navigation/AppNavigator.js
+++ app/Navigation/AppNavigator.js
@@ -1,16 +1,39 @@
-<Stack.Navigator
-  initialRouteName="Home"
-  screenOptions={{
-    headerShown: false,
-  }}
->
+// /app/navigation/AppNavigator.js
 
-  <Stack.Screen name="Home" component={HomeScreen} />
-  <Stack.Screen name="Scan" component={ScanScreen} />
-  <Stack.Screen name="Analysis" component={AnalysisScreen} />
-  <Stack.Screen name="Results" component={ResultsScreen} />
-  <Stack.Screen name="Dashboard" component={DashboardScreen} />
-  <Stack.Screen name="Settings" component={SettingsScreen} />
-  <Stack.Screen name="BlackNode" component={J3TM1BBlackNodeScreen} />
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 
-</Stack.Navigator>
+import HomeScreen from "../screens/HomeScreen";
+import ScanScreen from "../screens/ScanScreen";
+import AnalysisScreen from "../screens/AnalysisScreen";
+import ResultsScreen from "../screens/ResultsScreen";
+import DashboardScreen from "../screens/DashboardScreen";
+import SettingsScreen from "../screens/SettingsScreen";
+import J3TM1BBlackNodeScreen from "../components/system/J3TM1BBlackNodeScreen";
+
+import { registerNavigator } from "./BlackNodeRoute";
+
+const Stack = createStackNavigator();
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer ref={(nav) => registerNavigator(nav)}>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+          headerShown: false,
+          gestureEnabled: true,
+        }}
+      >
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Scan" component={ScanScreen} />
+        <Stack.Screen name="Analysis" component={AnalysisScreen} />
+        <Stack.Screen name="Results" component={ResultsScreen} />
+        <Stack.Screen name="Dashboard" component={DashboardScreen} />
+        <Stack.Screen name="Settings" component={SettingsScreen} />
+        <Stack.Screen name="BlackNode" component={J3TM1BBlackNodeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
