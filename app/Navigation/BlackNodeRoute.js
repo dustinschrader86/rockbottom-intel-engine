4ee70bb80a27d120247753ded600a26b5f5cfa20
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-13 21:56:56 -0400

    Add navigation functions for BlackNode route

--- app/Navigation/BlackNodeRoute.js
+++ app/Navigation/BlackNodeRoute.js
@@ -1,1 +1,25 @@
+// /app/navigation/BlackNodeRoute.js
+
+import { CommonActions } from "@react-navigation/native";
+
+let navigatorRef = null;
+
+export function registerNavigator(ref) {
+  navigatorRef = ref;
+}
+
+export function openBlackNode() {
+  if (!navigatorRef) return;
+
+  navigatorRef.dispatch(
+    CommonActions.navigate({
+      name: "BlackNode"
+    })
+  );
+}
+
+export default {
+  registerNavigator,
+  openBlackNode
+};
 
