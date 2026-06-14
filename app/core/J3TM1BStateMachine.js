dc083d62db0c08c396dfdce4963c80a5bfbb24ba
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-13 21:48:26 -0400

    Remove state machine logic from J3TM1BStateMachine.js
    Removed state machine logic for event handling.

--- app/core/J3TM1BStateMachine.js
+++ app/core/J3TM1BStateMachine.js
@@ -1,37 +1,3 @@
-state = Dormant
-
-onUserAction(event):
-    if state == Dormant:
-        if event == longPress(3.1s): state = AnomalyDetected
-        elif event == appOpen: state = Listening
-
-    if state == Listening:
-        if event == rapidTap(3): state = Observing
-        elif idle(30s): state = Dormant
-
-    if state == Observing:
-        if event == sequenceStart: state = Handshake
-        elif event == midnight: state = AnomalyDetected
-
-    if state == AnomalyDetected:
-        showGlitchMessage()
-        state = Cooldown
-
-    if state == Handshake:
-        if event == cipherCorrect: state = Awakened
-        elif timeout(10s): state = Observing
-
-    if state == Awakened:
-        if event == cipherCorrect: state = BlackNode
-        else: state = Cooldown
-
-    if state == BlackNode:
-        runBlackNodeScript()
-        state = Cooldown
-
-    if state == Cooldown:
-        wait(cooldownTime)
-        state = Dormant
 // /app/core/J3TM1BStateMachine.js
 
 import Messages from "../data/J3TM1BMessages";
@@ -174,4 +140,3 @@ class J3TM1BStateMachine {
 }
 
 export default new J3TM1BStateMachine();
-
