8086c60a859a004987253c985a63c2ed4ce9d10f
dustinschrader86 <33939240+dustinschrader86@users.noreply.github.com>
2026-06-13 22:27:51 -0400

    Update AppNavigator.js

--- app/Navigation/AppNavigator.js
+++ app/Navigation/AppNavigator.js
@@ -1,1 +1,16 @@
-<Stack.Screen name="Dashboard" component={DashboardScreen} />
+<Stack.Navigator
+  initialRouteName="Home"
+  screenOptions={{
+    headerShown: false,
+  }}
+>
+
+  <Stack.Screen name="Home" component={HomeScreen} />
+  <Stack.Screen name="Scan" component={ScanScreen} />
+  <Stack.Screen name="Analysis" component={AnalysisScreen} />
+  <Stack.Screen name="Results" component={ResultsScreen} />
+  <Stack.Screen name="Dashboard" component={DashboardScreen} />
+  <Stack.Screen name="Settings" component={SettingsScreen} />
+  <Stack.Screen name="BlackNode" component={J3TM1BBlackNodeScreen} />
+
+</Stack.Navigator>
